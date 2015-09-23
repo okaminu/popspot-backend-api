11d61e46b83b0e350e4d77fe133874d4da49a43c
Aurimas Degutis <aurimasdgt@gmail.com>
2015-09-23 23:25:25 +0300

    init

---
+++ src/main/java/com/pulsarsoft/popspot/Entity/Greeting.java
@@ -0,0 +1,22 @@
+package com.pulsarsoft.popspot.Entity;
+
+/**
+ * @author Aurimas Degutis.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
