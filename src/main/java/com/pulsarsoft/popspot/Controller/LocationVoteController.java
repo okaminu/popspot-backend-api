da347e15aea82314200b1e870905636d2f76c7d4
Aurimas Degutis <aurimasdgt@gmail.com>
2015-10-02 22:50:12 +0300

    app submits votes to service

--- src/main/java/com/pulsarsoft/popspot/Controller/LocationVoteController.java
+++ src/main/java/com/pulsarsoft/popspot/Controller/LocationVoteController.java
@@ -3,6 +3,7 @@ package com.pulsarsoft.popspot.Controller;
 import com.pulsarsoft.popspot.Model.LocationVote;
 import com.pulsarsoft.popspot.Service.LocationVoteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +23,12 @@ public class LocationVoteController {
     @RequestMapping(value = "/get-ratings", method = RequestMethod.GET)
     public Collection<LocationVote> getFeedback() {
         return locationVoteService.createMock();
+    }
+
+
+    @RequestMapping(value = "/put-rating", method = RequestMethod.POST)
+    public void putFeedback(@RequestBody String text)
+    {
+
     }
 }
